91b2acf411e1ea08cdd9cfe8fdd01a798b583259
ntpq <iqsnsd@gmail.com>
2024-12-09 00:20:01 +1300

    Day 1: Hello TypScript

---
+++ day1/hello.js
@@ -0,0 +1,7 @@
+var age = 25;
+var Name = "John";
+var isStudent = true;
+var anything = "Can be anything";
+console.log("Age: ".concat(age, ", Name: ").concat(Name, " , Is Student: ").concat(isStudent));
+anything = 42;
+console.log(anything);
---
+++ day1/hello.ts
@@ -0,0 +1,8 @@
+let age: number = 25;
+let Name: string = "John";
+let isStudent: boolean = true;
+let anything: any = "Can be anything";
+
+console.log(`Age: ${age}, Name: ${Name} , Is Student: ${isStudent}`);
+anything = 42;
+console.log(anything)
